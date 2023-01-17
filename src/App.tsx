0a1af0596a129a565db51dc977905b34420165e9
Lippiece <lippiece@proton.me>
2023-01-17 16:58:58 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./css/App.css";
+import Card from "./Card";
+
+const App = () => {
+  return (
+    <div className="cards">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Card.tsx
@@ -0,0 +1,19 @@
+const Card = () => {
+  return (
+    <div className="wrapper">
+      <div className="card">
+        <p>Сказочное заморское яство</p>
+        <h3>Нямушка</h3>
+        <h2>с фуа-гра</h2>
+        <p>10 порций</p>
+        <p>мышь в подарок</p>
+        <p>0,5</p>
+      </div>
+      <p>
+        Чего сидишь? Порадуй котэ, <a href="#">купи.</a>
+      </p>
+    </div>
+  );
+};
+
+export default Card;
